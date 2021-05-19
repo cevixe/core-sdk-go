4693c26eec4f895d7faec5d18c9634ab6de6b69e
Ronnie Ayala <ronnie.ayala@outlook.com>
2021-05-19 12:54:59 -0500

    Add custom ID support

--- cevixe/event.go
+++ cevixe/event.go
@@ -15,6 +15,16 @@ func NewDomainEvent(ctx context.Context, entity core.Entity, data interface{}, s
 	return factory.NewDomainEvent(ctx, data, entity, state)
 }
 
+func NewFirstDomainEvent(ctx context.Context, data interface{}, state interface{}) core.Event {
+	factory := ctx.Value(CevixeEventFactory).(core.EventFactory)
+	return factory.NewFirstDomainEvent(ctx, data, state)
+}
+
+func NewFirstDomainEventWithCustomID(ctx context.Context, id string, data interface{}, state interface{}) core.Event {
+	factory := ctx.Value(CevixeEventFactory).(core.EventFactory)
+	return factory.NewFirstDomainEventWithCustomID(ctx, data, id, state)
+}
+
 func NewBusinessEvent(ctx context.Context, data interface{}) core.Event {
 	factory := ctx.Value(CevixeEventFactory).(core.EventFactory)
 	return factory.NewBusinessEvent(ctx, data)
--- core/eventfactory.go
+++ core/eventfactory.go
@@ -6,4 +6,6 @@ type EventFactory interface {
 	NewCommandEvent(ctx context.Context, data interface{}) Event
 	NewBusinessEvent(ctx context.Context, data interface{}) Event
 	NewDomainEvent(ctx context.Context, data interface{}, entity Entity, state interface{}) Event
+	NewFirstDomainEvent(ctx context.Context, data interface{}, state interface{}) Event
+	NewFirstDomainEventWithCustomID(ctx context.Context, data interface{}, id string, state interface{}) Event
 }
