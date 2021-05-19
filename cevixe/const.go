6e5abfed861215dc260aa32911d40c0ae89133a3
Ronnie Ayala <ronnie.ayala@outlook.com>
2021-05-19 13:24:22 -0500

    Add default unique ID

--- cevixe/const.go
+++ cevixe/const.go
@@ -1,5 +1,6 @@
 package cevixe
 
+const CevixeUniqueEntityID string = "00000000-0000-0000-0000-000000000000"
 const CevixeEventTrigger string = "CevixeEventTrigger"
 const CevixeEventStore string = "CevixeEventStore"
 const CevixeStateStore string = "CevixeStateStore"
