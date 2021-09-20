56c2e15e0b17f16901d472027bc2f41b51c87a5c
Ronnie Ayala <ronnie.ayala@outlook.com>
2021-09-20 12:44:44 -0500

    Add common constants

--- cevixe/const.go
+++ cevixe/const.go
@@ -1,6 +1,8 @@
 package cevixe
 
 const CevixeUniqueEntityID string = "00000000-0000-0000-0000-000000000000"
+const CevixeTransaction string = "CevixeTransaction"
+const CevixeUserID string = "CevixeUserID"
 const CevixeEventTrigger string = "CevixeEventTrigger"
 const CevixeEventStore string = "CevixeEventStore"
 const CevixeStateStore string = "CevixeStateStore"
